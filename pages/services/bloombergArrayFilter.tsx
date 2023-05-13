422d2ff51c02e3336ff63175a9ea5e06d1a0ad1e
smmk <smmk123@gmail.com>
2023-05-13 12:48:22 -0400

    update array filter

--- pages/services/bloombergArrayFilter.tsx
+++ pages/services/bloombergArrayFilter.tsx
@@ -4,23 +4,23 @@ import React from 'react';
 const BloombergArrayFilter = (props: any) => {
     const modules = props.data.modules;
     const nonEmptyStories = modules
-    .map((module :any) => module.stories)
+    ?.map((module :any) => module.stories)
     .filter((stories:any) => stories.length > 0);
 
-  const allStories = nonEmptyStories.flat();
+  const allStories = nonEmptyStories?.flat();
 
-  const uniqueStories = allStories.reduce((acc: any, story: any) => {
+  const uniqueStories = allStories?.reduce((acc: any, story: any) => {
     if (!acc.find((s: any) => s.id === story.id)) {
       acc.push(story);
     }
     return acc;
   }, []);
   
-    const limitedStories = uniqueStories.slice(0, 10);
+    const limitedStories = uniqueStories?.slice(0, 10);
   
     return (
       <div>
-        {modules && limitedStories.map((story: any) => (
+        {modules && limitedStories?.map((story: any) => (
           <div className="m-2" key={story.id}>
             <h2 className="text-xl font-bold">{story.title}</h2>
             <p>{story.summary}</p>
